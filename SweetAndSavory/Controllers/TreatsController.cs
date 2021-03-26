a12cd455bee8034ef99a1fae1a2c619e44c40bd4
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-26 11:14:51 -0700

    add index route

---
+++ SweetAndSavory/Controllers/TreatsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using SweetAndSavory.Models;
+
+namespace SweetAndSavory.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly SweetAndSavoryContext _db;
+
+    public TreatsController(SweetAndSavoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Treat> model = _db.Treats.ToList();
+    }
+  }
+}
